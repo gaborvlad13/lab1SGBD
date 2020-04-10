3957894bf2fef32c50de7a283c824bc9999e6337
gaborvlad13 <gabor.vlad13@gmail.com>
2020-04-10 03:15:50 +0300

    Initial commit

---
+++ SGBD/SGBD/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SGBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxNrCamera = new System.Windows.Forms.TextBox();
+            this.textBoxEtajCamera = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxStudenti = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            this.buttonConnect.Location = new System.Drawing.Point(28, 22);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(132, 50);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            this.dataGridView1.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 112);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(354, 271);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView2.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(405, 112);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(354, 271);
+            this.dataGridView2.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(28, 418);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 78);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Display";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonDelete.Location = new System.Drawing.Point(184, 418);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(132, 78);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            this.buttonUpdate.Location = new System.Drawing.Point(342, 418);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(132, 78);
+            this.buttonUpdate.TabIndex = 4;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            this.buttonAdd.Location = new System.Drawing.Point(504, 418);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(132, 78);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            this.textBoxId.Location = new System.Drawing.Point(275, 31);
+            this.textBoxId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(53, 27);
+            this.textBoxId.TabIndex = 6;
+            this.textBoxNrCamera.Location = new System.Drawing.Point(440, 30);
+            this.textBoxNrCamera.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxNrCamera.Name = "textBoxNrCamera";
+            this.textBoxNrCamera.Size = new System.Drawing.Size(55, 27);
+            this.textBoxNrCamera.TabIndex = 7;
+            this.textBoxEtajCamera.Location = new System.Drawing.Point(614, 30);
+            this.textBoxEtajCamera.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxEtajCamera.Name = "textBoxEtajCamera";
+            this.textBoxEtajCamera.Size = new System.Drawing.Size(52, 27);
+            this.textBoxEtajCamera.TabIndex = 8;
+            this.label1.Location = new System.Drawing.Point(192, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 22);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Camera id";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label2.Location = new System.Drawing.Point(351, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 22);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nr camera";
+            this.label3.Location = new System.Drawing.Point(519, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 22);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Etaj camera";
+            this.label4.Location = new System.Drawing.Point(691, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 22);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Nr Studenti";
+            this.textBoxStudenti.Location = new System.Drawing.Point(786, 30);
+            this.textBoxStudenti.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxStudenti.Name = "textBoxStudenti";
+            this.textBoxStudenti.Size = new System.Drawing.Size(52, 27);
+            this.textBoxStudenti.TabIndex = 13;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 562);
+            this.Controls.Add(this.textBoxStudenti);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEtajCamera);
+            this.Controls.Add(this.textBoxNrCamera);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonConnect);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxNrCamera;
+        private System.Windows.Forms.TextBox textBoxEtajCamera;
+        private System.Windows.Forms.TextBox textBoxStudenti;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ SGBD/SGBD/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGBD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs =
+            new SqlConnection(
+                @"Data Source=DESKTOP-IS7SFF6\SQLEXPRESS; Initial Catalog=Camine_Tema_3; Integrated Security=True");
+
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        BindingSource bs = new BindingSource();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM CAMINE", cs);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            bs.DataSource = ds.Tables[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM Camere WHERE CamineId=@id", cs);
+            da.SelectCommand.Parameters.Add("@id", SqlDbType.Int).Value =
+                Convert.ToInt32(dataGridView1.CurrentCell.Value);
+            ds2.Clear();
+            da.Fill(ds2);
+            dataGridView2.DataSource = ds2.Tables[0];
+            bs.DataSource = ds2.Tables[0];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            da.UpdateCommand =
+                new SqlCommand("Update Camere set NrCamera=@nrCamera, EtajCamera=@etajCamera, NrStudenti=@nrStudenti where CamereId=@id", cs);
+            da.UpdateCommand.Parameters.Add("@nrCamera", SqlDbType.Int).Value = Convert.ToInt32(textBoxNrCamera.Text);
+            da.UpdateCommand.Parameters.Add("@etajCamera", SqlDbType.Int).Value =
+                Convert.ToInt32(textBoxEtajCamera.Text);
+            da.UpdateCommand.Parameters.Add("@nrStudenti", SqlDbType.Int).Value = Convert.ToInt32(textBoxStudenti.Text);
+            da.UpdateCommand.Parameters.Add("@id", SqlDbType.Int).Value =
+                Convert.ToInt32(dataGridView2.CurrentCell.Value);
+            cs.Open();
+            var x = da.UpdateCommand.ExecuteNonQuery();
+            cs.Close();
+            if (x >= 1)
+            {
+                MessageBox.Show("The record has been updated");
+            }
+
+            ds2.Clear();
+            da.Fill(ds2);
+            dataGridView2.DataSource = ds2.Tables[0];
+            bs.DataSource = ds2.Tables[0];
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dr;
+            dr = MessageBox.Show("Are you sure?", "Confirm Deletion", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                da.DeleteCommand = new SqlCommand("Delete from Camere where CamereId=@id", cs);
+                da.DeleteCommand.Parameters.Add("@id", SqlDbType.Int).Value =
+                    Convert.ToInt32(dataGridView2.CurrentCell.Value);
+                cs.Open();
+                da.DeleteCommand.ExecuteNonQuery();
+                cs.Close();
+                ds2.Clear();
+                da.Fill(ds2);
+                dataGridView2.DataSource = ds2.Tables[0];
+                bs.DataSource = ds2.Tables[0];
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            da.InsertCommand = new SqlCommand("INSERT INTO Camere VALUES (@cameraId, @camineId, @nrCamera, @etajCamera, @nrStudenti)", cs);
+            da.InsertCommand.Parameters.Add("@cameraId", SqlDbType.Int).Value = Convert.ToInt32(textBoxId.Text);
+            da.InsertCommand.Parameters.Add("@camineId", SqlDbType.Int).Value =
+                Convert.ToInt32(dataGridView1.CurrentCell.Value);
+            da.InsertCommand.Parameters.Add("@nrCamera", SqlDbType.Int).Value = Convert.ToInt32(textBoxNrCamera.Text);
+            da.InsertCommand.Parameters.Add("@etajCamera", SqlDbType.Int).Value = Convert.ToInt32(textBoxEtajCamera.Text);
+            da.InsertCommand.Parameters.Add("@nrStudenti", SqlDbType.Int).Value = Convert.ToInt32(textBoxStudenti.Text);
+            cs.Open();
+            da.InsertCommand.ExecuteNonQuery();
+            cs.Close();
+            ds2.Clear();
+            da.Fill(ds2);
+            dataGridView2.DataSource = ds2.Tables[0];
+            bs.DataSource = ds2.Tables[0];
+        }
+    }
+}
